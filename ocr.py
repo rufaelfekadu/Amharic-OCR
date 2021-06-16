ed13a1aab0c82dae5fe0b38f8fcb7159382d30d2
Rufael Fekadu <rufaelfekadu48@gmail.com>
2021-06-16 21:12:32 +0300

    init files added

--- ocr.py
+++ ocr.py
@@ -4,6 +4,8 @@ import pytesseract
 import argparse
 import cv2
 import os
+import imutils
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
@@ -15,25 +17,28 @@ args = vars(ap.parse_args())
 # load the example image and convert it to grayscale
 image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# check to see if we should apply thresholding to preprocess the
-# image
+
+# check to see if we should apply thresholding to preprocess the image
 if args["preprocess"] == "thresh":
 	gray = cv2.threshold(gray, 0, 255,
 		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-# make a check to see if median blurring should be done to remove
-# noise
+
+# make a check to see if median blurring should be done to remove noise
 elif args["preprocess"] == "blur":
 	gray = cv2.medianBlur(gray, 3)
-# write the grayscale image to disk as a temporary file so we can
-# apply OCR to it
+
+# write the grayscale image to disk as a temporary file so we can apply OCR to it
 filename = "{}.png".format(os.getpid())
 cv2.imwrite(filename, gray)
 
-# load the image as a PIL/Pillow image, apply OCR, and then delete
-# the temporary file
+# load the image as a PIL/Pillow image, apply OCR, and then delete the temporary file
 text = pytesseract.image_to_string(Image.open(filename),lang='amh')
 os.remove(filename)
-print(text)
+
+file1 = open("ocr_temp.txt","w+")
+file1.write(text)
+file1.close()
+
 # show the output images
 cv2.imshow("Image", image)
 cv2.imshow("Output", gray)
