f9123e6941f36c37b404469632649826253e639b
unknown <leykunejigue@gmail.com>
2021-06-17 14:27:09 +0300

    intital

---
+++ flask_module/app.py
@@ -0,0 +1,65 @@
+import os
+import cv2
+
+from flask import Flask, render_template, request, flash, url_for, send_from_directory
+from werkzeug.utils import redirect, secure_filename
+
+UPLOAD_FOLDER = 'static/uploads/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+#
+#
+# @app.route('/', methods=['POST', 'Get'])
+# def home():
+#     if request.method == 'GET':
+#         tasks = "hello"
+#         return render_template("index.html", tasks=tasks)
+
+
+@app.route('/about')
+def about():
+    return "welcome about"
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            # flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            # flash('No selected file')
+            print("images not  saved")
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            print(app.config['UPLOAD_FOLDER'])
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            tasks = file.filename
+            print("images saved")
+            return render_template('index.html', tasks='uploads/'+tasks)
+            # return redirect(url_for('download_file', name=filename))
+    tasks = "upload images"
+    return render_template('index.html', tasks=tasks)
+
+
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- ocr.py
+++ ocr.py
@@ -4,13 +4,15 @@ import pytesseract
 import argparse
 import cv2
 import os
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image to be OCR'd")
+ap.add_argument("-i", "--image",
+                help="path to input image to be OCR'd",type=str,default="images/image1.png")
 ap.add_argument("-p", "--preprocess", type=str, default="thresh",
-	help="type of preprocessing to be done")
+                help="type of preprocessing to be done")
 args = vars(ap.parse_args())
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # load the example image and convert it to grayscale
 image = cv2.imread(args["image"])
@@ -18,12 +20,12 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # check to see if we should apply thresholding to preprocess the
 # image
 if args["preprocess"] == "thresh":
-	gray = cv2.threshold(gray, 0, 255,
-		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    gray = cv2.threshold(gray, 0, 255,
+                         cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 # make a check to see if median blurring should be done to remove
 # noise
 elif args["preprocess"] == "blur":
-	gray = cv2.medianBlur(gray, 3)
+    gray = cv2.medianBlur(gray, 3)
 # write the grayscale image to disk as a temporary file so we can
 # apply OCR to it
 filename = "{}.png".format(os.getpid())
@@ -31,7 +33,7 @@ cv2.imwrite(filename, gray)
 
 # load the image as a PIL/Pillow image, apply OCR, and then delete
 # the temporary file
-text = pytesseract.image_to_string(Image.open(filename),lang='amh')
+text = pytesseract.image_to_string(Image.open(filename), lang='amh')
 os.remove(filename)
 print(text)
 # show the output images
