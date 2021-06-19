5720a70b3f9a870b5bc48a41a32e725fa795d8da
unknown <leykunejigue@gmail.com>
2021-06-19 07:13:31 +0300

    ui updated

--- flask_module/ocr.py
+++ flask_module/ocr.py
@@ -7,16 +7,14 @@ import cv2
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from docx import Document
-from docx.shared import Inches
 
 
 def display(im_path):
     dpi = 80
     im_data = plt.imread(im_path)
 
-    height, width  = im_data.shape[:2]
-    
+    height, width = im_data.shape[:2]
+
     # What size does the figure need to be in inches to fit the image?
     figsize = width / float(dpi), height / float(dpi)
 
@@ -32,9 +30,11 @@ def display(im_path):
 
     plt.show()
 
+
 def grayscale(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
+
+
 def noise_removal(image):
     import numpy as np
     kernel = np.ones((1, 1), np.uint8)
@@ -45,21 +45,24 @@ def noise_removal(image):
     image = cv2.medianBlur(image, 3)
     return (image)
 
+
 def thin_font(image):
     import numpy as np
     image = cv2.bitwise_not(image)
-    kernel = np.ones((2,2),np.uint8)
+    kernel = np.ones((2, 2), np.uint8)
     image = cv2.erode(image, kernel, iterations=1)
     image = cv2.bitwise_not(image)
     return (image)
 
+
 def thick_font(image):
     import numpy as np
     image = cv2.bitwise_not(image)
-    kernel = np.ones((2,2),np.uint8)
+    kernel = np.ones((2, 2), np.uint8)
     image = cv2.dilate(image, kernel, iterations=1)
     image = cv2.bitwise_not(image)
     return (image)
+
 
 def getSkewAngle(cvImage) -> float:
     # Prep image, copy, convert to gray scale, blur, and threshold
@@ -76,15 +79,15 @@ def getSkewAngle(cvImage) -> float:
 
     # Find all contours
     contours, hierarchy = cv2.findContours(dilate, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key = cv2.contourArea, reverse = True)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
     for c in contours:
         rect = cv2.boundingRect(c)
-        x,y,w,h = rect
-        cv2.rectangle(newImage,(x,y),(x+w,y+h),(0,255,0),2)
+        x, y, w, h = rect
+        cv2.rectangle(newImage, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     # Find largest contour and surround in min area box
     largestContour = contours[0]
-    print (len(contours))
+    print(len(contours))
     minAreaRect = cv2.minAreaRect(largestContour)
     cv2.imwrite("temp/boxes.jpg", newImage)
     # Determine the angle. Convert it to the value that was originally used to obtain skewed image
@@ -92,6 +95,8 @@ def getSkewAngle(cvImage) -> float:
     if angle < -45:
         angle = 90 + angle
     return -1.0 * angle
+
+
 # Rotate the image around its center
 def rotateImage(cvImage, angle: float):
     newImage = cvImage.copy()
@@ -101,6 +106,7 @@ def rotateImage(cvImage, angle: float):
     newImage = cv2.warpAffine(newImage, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
     return newImage
 
+
 def deskew(cvImage):
     angle = getSkewAngle(cvImage)
     return rotateImage(cvImage, -1.0 * angle)
@@ -108,38 +114,38 @@ def deskew(cvImage):
 
 def remove_borders(image):
     contours, heiarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cntsSorted = sorted(contours, key=lambda x:cv2.contourArea(x))
+    cntsSorted = sorted(contours, key=lambda x: cv2.contourArea(x))
     cnt = cntsSorted[-1]
     x, y, w, h = cv2.boundingRect(cnt)
-    crop = image[y:y+h, x:x+w]
+    crop = image[y:y + h, x:x + w]
     return (crop)
 
+
 def preprocess(image):
-    
     image = deskew(image)
     image = grayscale(image)
     # thresh = cal_thresh(image)
     # thresh, image = cv2.threshold(image, 80, 180, cv2.THRESH_BINARY)
     # blur = cv2.GaussianBlur(image,(5,5),0)
     blur = cv2.medianBlur(image, 5)
-    ret3,image = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    ret3, image = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     # th = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 21, 2)
     # image =noise_removal(th)
     return image
 
-def ocr(image,scanned = False):
-    
+
+def ocr(image, scanned=False,language=''):
     if scanned:
         pre_img = image
     else:
         pre_img = preprocess(image)
-    
+
     # filename = "{}.png".format(os.getpid())
     # cv2.imwrite(filename, pre_img)
     # display(filename)
     # cv2.imshow('image',pre_img)
     # load the image as a PIL/Pillow image, apply OCR, and then delete the temporary file
-    text = pytesseract.image_to_string(pre_img,lang='amh+eng')
+    text = pytesseract.image_to_string(pre_img, lang=language)
     # os.remove(filename)
     # _ , bound_box = pytesseract.image_to_boxes(pre_img,lang='amh+eng')
 
@@ -153,9 +159,11 @@ def ocr(image,scanned = False):
     # document.save(str(os.getpid())+'.docx')
 
     filename = "{}.txt".format(os.getpid())
-    dir = os.path.join('output',filename)
-    file1 = open(dir,"w")
-    file1.write(text)
-    file1.close()
+    dir = os.path.join('../output', filename)
+    # file1 = open(dir, "w")
+    # file1.write(text)
+    # file1.close
+    with open(dir, "w", encoding="utf-8") as f:
+        f.write(text)
 
-    return text,dir
+    return text, filename
